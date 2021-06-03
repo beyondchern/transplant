2c55056d0fe2d9a669409a34e2d2d90d82818bd6
beyondchern <beyondchern@gmail.com>
2021-06-03 08:37:58 +0200

    intro text update

--- botanical_encounter/script/button_pagelinks.js
+++ botanical_encounter/script/button_pagelinks.js
@@ -1,15 +1,15 @@
-document.getElementById("openPage_map").addEventListener("click", function () {
+document.getElementById("openPage_map").onclick = function () {
   window.open("pages/map.html", "_self");
-});
+};
 
-document
-  .getElementById("openPage_johannesDrawings")
-  .addEventListener("click", function () {
-    window.open("pages/johannes_drawings.html", "_self");
-  });
+document.getElementById("openPage_map2").onclick = function () {
+  window.open("pages/map.html", "_self");
+};
 
-document
-  .getElementById("openPage_theophilPrints")
-  .addEventListener("click", function () {
-    window.open("pages/theophil_prints.html", "_self");
-  });
+document.getElementById("openPage_johannesDrawings").onclick = function () {
+  window.open("pages/johannes_drawings.html", "_self");
+};
+
+document.getElementById("openPage_theophilPrints").onclick = function () {
+  window.open("pages/theophil_prints.html", "_self");
+};
