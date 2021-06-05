3737bcd3b851fbfce439848df67b5c31d294dcbc
beyondchern <beyondchern@gmail.com>
2021-06-05 15:09:06 +0200

    share testing 2

--- botanical_encounter/script/sharedata.js
+++ botanical_encounter/script/sharedata.js
@@ -1,1 +1,17 @@
+const shareData = {
+  title: "Botanical Encounter",
+  text: "Botanical Encounter is a collaborative mapping project inspired by a 16th century naturalist book Codex Kentmanus",
+  url: "https://botanical-encounter.herokuapp.com/pages/map.html",
+};
 
+const btn = document.getElementById("shareBtton");
+
+// Must be triggered some kind of "user activation"
+btn.addEventListener("click", async () => {
+  try {
+    await navigator.share(shareData);
+    console.log(shareData);
+  } catch (err) {
+    console.log("Error: " + err);
+  }
+});
