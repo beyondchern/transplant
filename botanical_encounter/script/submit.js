0b7687447c49a237dc0d958725921ab8813071e6
beyondchern <beyondchern@gmail.com>
2021-06-11 19:11:58 +0200

    http to https

--- botanical_encounter/script/submit.js
+++ botanical_encounter/script/submit.js
@@ -1,5 +1,5 @@
 function submit() {
-  fetch("http://beyondchern.eu.pythonanywhere.com/submit", {
+  fetch("https://beyondchern.eu.pythonanywhere.com/submit", {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify(submission),
