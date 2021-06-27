21968b539451381840280b8614cafd253cb960d6
beyondchern <beyondchern@gmail.com>
2021-06-27 22:47:08 +0200

    update chinese

--- script/get_lang_value.js
+++ script/get_lang_value.js
@@ -2,6 +2,6 @@ let lang = getQueryStringValue("lang");
 if (lang == "") {
   lang = navigator.language.substr(0, 2);
 }
-if (!["en", "de"].includes(lang)) {
+if (!["en", "de", "zh"].includes(lang)) {
   lang = "en";
 }
--- script/language_data.js
+++ script/language_data.js
@@ -16,15 +16,15 @@ languages = {
       "What is your most memorable encounter with plants?<br />Where did you meet them?<br />Were you moved by their stunning colors, unforgettable fragrance, or magical healing power?<br /><br />Let’s create a map with your arts and stories of human-plant encounter!<br />",
     intro_first_button: "See the map >>",
     intro_codex_innerhtml:
-      '<i>Botanical Encounter</i> is a collaborative mapping project inspired by the 16-century German naturalist book Codex Kentmanus.<br /><br />The book contains one of the earliest collections of botanical arts. It consists of hundreds of illustrations from physician Johannes Kentmann, prints by his son Theophil Kentmann, and contributions by many artists and scholars through exchanges and collaborations.<br /><br />It is a book of art and science, celebrating human-plant encounters and representing knowledge exchange.<br /><br /><a href="https://ausstellungen.deutsche-digitale-bibliothek.de/codex-kentmanus/#s0" target="_blank">Visit <b>Anna Amalia Bibliothek’s website</b> to learn more about Codex Kentmanus</a>',
+      '<i>Botanical Encounter</i> is a collaborative mapping project inspired by the 16-century naturalist book Codex Kentmanus.<br /><br />The book contains one of the earliest collections of botanical arts. It consists of hundreds of illustrations from the German physician Johannes Kentmann, prints by his son Theophil Kentmann, and contributions by many artists and scholars through exchanges and collaborations.<br /><br />It is a book of art and science, celebrating human-plant encounter and knowledge exchange.<br /><br /><a href="https://ausstellungen.deutsche-digitale-bibliothek.de/codex-kentmanus/#s0" target="_blank">Visit <b>Anna Amalia Bibliothek’s website</b> to learn more about Codex Kentmanus</a>',
     intro_johannes:
-      "German physician Johannes Kentmann’s plant drawings, illustrate the plants he encountered in Padua Botanical Garden during his study in Italy.<br /><br />There, Johannes met the plants he didn’t know before. At his time, they were rarely depicted in the north of the Alps. Some of them were not even from Europe. They traveled to Italy through international trade.<br /><br />Meeting these new plants, Johannes started to observe and, with his paintbrushes, document them in detail. His arts become one of the earliest collections of botanical illustrations.<br />",
+      "Johannes Kentmann’s plant drawings, illustrate the plants he encountered in Padua Botanical Garden during his study in Italy.<br /><br />There, Johannes met the plants he had not known before. In his time, they were rarely depicted in the north of the Alps. Some of them were not even from Europe. They traveled to Italy through international trade.<br /><br />Meeting these new plants, Johannes started to observe and paint them in detail. His artworks became one of the earliest collections of botanical illustrations.<br />",
     intro_johannes_button: "Explore Johannes Kentmann’s plant drawings >>",
     intro_theophil_innerhtml:
-      "On the other hand, Johannes’ son Theophil Kentmann chose to document his local plant acquaintances in Germany with a new technique -- natural self-printing (Naturselbstdruck).<br /><br />Plant parts were colored with black soot or charcoal ink, then rubbed by hand onto a paper. The result prints were later colored with verdigris solution and other pigments.<br />",
+      "Inspired by his father's works, Johannes’ son Theophil Kentmann also started to document his local plant acquaintances in Germany with a new technique -- natural self-printing (Naturselbstdruck).<br /><br />Plant parts were colored with black soot or charcoal ink, then rubbed by hand onto a paper. The resulting prints were later colored with verdigris solution and other pigments.<br />",
     intro_theophil_button: "Explore Theophil Kentmann’s plant prints >>",
     intro_final:
-      "And you?<br /><br />How do botanical encounters spark your creativity?",
+      "And you?<br /><br />How do botanical encounters spark your creativity?</br>Draw, print the plants and write down your story.",
     intro_final_button: "Share them on the map! >>",
 
     dialog_title: "Share this story",
@@ -110,5 +110,59 @@ languages = {
     preview_author: 'Von <span id="previewAuthor"></span>',
     edit_button: "← Bearbeiten",
     submit_button: "Reichen Sie Ihre Geschichte ein",
+  },
+
+  zh: {
+    page_title_index: "Botanical Encounter",
+    page_title_map: "遇見植物地圖",
+    page_title_johannes: "Johannes Kentmann的植物繪畫",
+    page_title_theophil: "Theophil Kentmann的植物版畫",
+    page_title_contact: "與我聯絡",
+
+    nav_index: "Botanical Encounter",
+    nav_map: "遇見植物地圖",
+    nav_johannes: "Johannes Kentmann的植物繪畫",
+    nav_theophil: "Theophil Kentmann的植物版畫",
+    nav_contact: "與我聯絡",
+
+    intro_first_innerhtml:
+      "哪些植物讓你印象深刻?<br />你在哪裡遇見他們的?<br />是他們奪目的顏色、令人難忘的氣味、或是他們的神奇的療癒力量讓你難忘?<br /><br />讓我們一起繪製地圖，用藝術創作記錄下這些人與植物相遇的故事吧!<br />",
+    intro_first_button: "前往地圖 >>",
+    intro_codex_innerhtml:
+      '<i>Botanical Encounter</i> 是一個協作植物地圖。靈感來源是一本十六世紀的博物學書籍 《肯曼之書 Codex Kentmanus》。<br /><br />肯曼之書中的植物圖集，是世界上最早的植物繪圖之一，裡頭包含上百張德國醫師約翰納．肯曼(Johannes Kentmann)的手繪圖、以及其子提奧菲．肯曼(Theophil Kentmann)的版畫。知識交流與合作下，許多當時歐陸藝術家及學者的作品也被收在圖集內。<br /><br />肯曼之書是一本藝術與科學之書，記載了人與植物相遇、以及知識交流的美好。<br /><br /><a href="https://ausstellungen.deutsche-digitale-bibliothek.de/codex-kentmanus/#s0" target="_blank">前往<b>德國安娜．阿瑪麗亞圖書館(Anna Amalia Bibliothek)</b>的網站瞭解更多肯曼之書的資訊 >></a>',
+    intro_johannes:
+      "德國醫師約翰納．肯曼的植物手繪圖，描繪了他在義大利留學時，在帕多瓦(Padua)植物園裡遇見的植物們。<br /><br />這些植物由於遇阿爾卑斯山屏障，難往北邊傳播，因此也極少被描繪。許多植物甚至不產於歐陸，而是透過國際貿易落腳義大利。因此，在帕多瓦植物園裡，約翰納遇見他從來不認識的植物們。<br /><br />與不相識的植物相遇，讓約翰納開始觀察、並用畫筆仔細記錄下這些植物的樣貌。這些繪畫作品，後來成為最早的植物繪圖之一。<br />",
+    intro_johannes_button: "欣賞更多翰納．肯曼的手繪圖 >>",
+    intro_theophil_innerhtml:
+      "受到父親的啟發，約翰納的兒子提菲奧．肯曼則選擇用「自然物自身印刷法 (Naturselbstdruck)」記錄他身邊熟識的德國在地植物們。<br />這種版畫製作方法，是在植物體本身抹上黑色煤烟或煤炭墨水後，以手揉印至紙上完成。印製完成後，再以銅綠溶液或其他顏料加以著色。",
+    intro_theophil_button: "欣賞更多提菲奧．肯曼的植物版畫 >>",
+    intro_final: "你呢?<br /><br />與植物相遇是不是也激發了你的創作力?",
+    intro_final_button: "在地圖上跟大家分享你的創作吧！>>",
+
+    dialog_title: "分享這個故事",
+    copy_link_button: "複製網址",
+    share_button: "分享",
+
+    welcome_pop_up:
+      "<h1>歡迎來到植物故事地圖！</h1><br>請點選任意照片來閱讀人與植物的故事，<br>或將這個對話框(對，就是我)移到你與植物相遇的地方，開始編寫你們的故事 :)",
+    upload_pop_up:
+      '<button class="button" id="uploadPhotoButton">上傳作品照片</button><p>上傳作品，開始分享你的故事! <br>你也可以再次移動這個標籤到你與植物相遇的地方。</p>',
+    back_to_map_button: "← 返回地圖",
+    crop_img_button: "裁切照片 →",
+
+    input_plant_name_label:
+      '<label class="input_label" for="inputPlantName">植物俗名</label><input type="text" class="input_field adding__form_text_input_plant_name" id="inputPlantName" placeholder="你怎麼稱呼這株植物的?" required="required" />',
+    input_scientific_name_label:
+      '<label class="input_label" for="inputScientificName" >植物學名</label><input type="text" class="input_field adding__form_text_input_scientific_name" id="inputScientificName" placeholder="(非必填)"/>',
+    input_author_label:
+      '<label class="input_label" for="inputAuthor" >作者</label><input type="text" class="input_field adding__form_text_input_author" id="inputAuthor" placeholder="你的名字" required="required" />',
+    input_story_label:
+      '<label class="input_label" for="inputStory">植物與人的故事</label><textarea class="input_field adding__form_text_input_story" id="inputStory" placeholder="告訴我們你的創作，以及作品背後，你與植物的相遇!" rows="14" cols="10" wrap="soft" style="overflow: scroll; resize: none" required="required"></textarea>',
+    recrop_button: "← 重新裁切照片",
+    preview_button: "預覽 →",
+
+    preview_author: '<span id="previewAuthor"></span>',
+    edit_button: "← 編輯故事",
+    submit_button: "完成了，上傳故事!",
   },
 };
