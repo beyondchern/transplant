62ef964eb2101e90bde59be14e8611c250ff2b99
beyondchern <beyondchern@gmail.com>
2022-02-08 18:00:01 +0100

    Added page headings

--- script/exhibition_data.js
+++ script/exhibition_data.js
@@ -19,8 +19,8 @@ exhibitions = {
     exhib_title: "Garage Plant drawing/printing workshop",
     exhib_date: "31st July 2021",
     exhib_location: "Missoula, MT, USA",
-    exhib_link: "http://garage-missoula.com/events/",
+    exhib_link: "http://garage-missoula.com/works/",
     exhib_description:
-      "Draw a plant, explore a simple printing method and become part of an international art project.",
+      "As part of the Botanical Encounter project, we had a workshop with international students and scholars in cooperation with our partner, the University of Montana (UM) Global Engagement Office and an open workshop in the garden of the sustainability project UM FLAT. Next to drawing plants, we explored the technique of nature printing that was used in part in 'the Codex Kentmanus'. For this purpose, we made a simple ink from soot, egg yolk and alcohol.",
   },
 };
